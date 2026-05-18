d5575a04172f8dc93966f0474e85e6b2fea7d818
sunHelen12 <sunrise.developer12@gmail.com>
2026-05-18 19:00:07 -0300

    fix: validacoes

--- backend/src/routes/produtoRoutes.js
+++ backend/src/routes/produtoRoutes.js
@@ -6,6 +6,7 @@ const produtoController = require("../controllers/produtoController");
 const validarCadastrarProduto = require("../middlewares/produto/validarCadastrarProduto");
 const validarAtualizarProduto = require("../middlewares/produto/validarAtualizarProduto");
 const validarDeletarProduto = require("../middlewares/produto/validarDeletarProduto");
+const validarBuscarProduto = require("../middlewares/produto/validarBuscarProduto");
 
 
 // Cadastrar (POST)
@@ -21,9 +22,6 @@ router.delete('/:uuid', validarDeletarProduto, produtoController.deletarProduto)
 router.get("/", produtoController.buscarTodosProdutos);
 
 // Buscar por id (GET)
-router.get(
-    '/:uuid',
-    produtoController.buscarProduto
-);
+router.get('/:uuid', validarBuscarProduto, produtoController.buscarProduto);
 
 module.exports = router;
