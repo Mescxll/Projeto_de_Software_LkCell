1e0970d4d3124bd132613de54d485177255faffa
sunHelen12 <sunrise.developer12@gmail.com>
2026-05-18 16:25:34 -0300

    fix: correcao de client para cliente

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const clientRoutes = require("./src/routes/clientRoutes");
+const clienteRoutes = require("./src/routes/clienteRoutes");
 const funcionarioRoutes = require("./src/routes/funcionarioRoutes");
 const produtoRoutes = require("./src/routes/produtoRoutes");
 const catalogoRoutes = require("./src/routes/catalogoRoutes");
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Lista das rotas configuradas
-app.use("/api/clientes", clientRoutes);
+app.use("/api/clientes", clienteRoutes);
 app.use("/api/funcionarios", funcionarioRoutes);
 app.use("/api/produtos", produtoRoutes);
 app.use("/api/catalogo", catalogoRoutes);
--- backend/src/routes/clienteRoutes.js
+++ backend/src/routes/clienteRoutes.js
@@ -1,35 +1,35 @@
 // Rotas Cliente
 const express = require('express');
 const router = express.Router();
-const clientController = require('../controllers/clientController');
+const clienteController = require('../controllers/clienteController');
 const validarCadastroCliente = require('../middlewares/validarCadastroCliente');
 
 //Rota do tipo POST para cadastro de clientes
 router.post(
     '/',
     validarCadastroCliente,
-    clientController.cadastrarCliente
+    clienteController.cadastrarCliente
 );
 
 // Rota do tipo GET para buscar cliente por CPF/CNPJ
 router.get(
     '/:uuid',
-    clientController.buscarCliente
+    clienteController.buscarCliente
 );
 
 // Rota do tipo GET para buscar todos os clientes
-router.get('/', clientController.buscarTodosClientes);
+router.get('/', clienteController.buscarTodosClientes);
 
 // Rota do tipo PUT para atualizar cliente por CPF/CNPJ
 router.put(
     '/:uuid',
-    clientController.atualizarCliente
+    clienteController.atualizarCliente
 );
 
 // Rota do tipo DELETE para remover cliente por CPF/CNPJ
 router.delete(
     '/:uuid',
-    clientController.deletarCliente
+    clienteController.deletarCliente
 );
 
 module.exports = router;
