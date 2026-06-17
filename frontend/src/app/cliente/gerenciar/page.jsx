022f3db685067a692d530b96f2335b90ac67e067
Gabryelle Duarte <gabryelleddev@gmail.com>
2026-06-17 18:03:16 -0300

    Update page.jsx
    add email at placeholder

--- frontend/src/app/cliente/gerenciar/page.jsx
+++ frontend/src/app/cliente/gerenciar/page.jsx
@@ -70,7 +70,7 @@ export default function GerenciarClientes() {
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
           <input
             type="text"
-            placeholder="Buscar por nome, ID, CPF ou CNPJ, Física ou Jurídica..."
+            placeholder="Buscar por nome, ID, email, CPF ou CNPJ, Física ou Jurídica..."
             className="w-full pl-9 pr-6 py-2 border border-gray-200 text-gray-800 rounded-lg bg-white text-sm focus:ring-2 focus:ring-blue-500 outline-none shadow-sm placeholder:text-gray-400"
             onChange={(e) => setBusca(e.target.value)}
           />
